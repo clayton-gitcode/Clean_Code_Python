fd19c3bc03dddabc549c787b33d9f25ae1b8b77f
Clayton Lima <_clayton@outlook>
2025-09-16 20:39:40 -0300

    feat: Implemneting main with process_handler with introduction page

---
+++ src/view/first_view.py
@@ -0,0 +1,14 @@
+
+
+def introduction_page()->str:
+    message = '''
+        Sistema Musical
+    * Cadastrar Musica - 1
+    * Crir Playlist - 2
+    * Sair - 5
+    '''
+
+    print(message)
+    command = input("Comando: ")
+
+    return command
